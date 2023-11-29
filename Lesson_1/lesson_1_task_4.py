0f105b7ea110f8ff7a1adbdefce94a6f2cfd9e7a
Alex Ananin <lxananin@gmail.com>
2023-11-29 21:00:27 +0500

    fixes

--- Lesson_1/lesson_1_task_4.py
+++ Lesson_1/lesson_1_task_4.py
@@ -1,4 +1,4 @@
 first_name = input()
 last_name = input()
 
-print ("Вас зовут: " + first_name + " " + last_name)
+print("Вас зовут:", first_name, last_name)
--- Lesson_1/lesson_1_task_6.py
+++ Lesson_1/lesson_1_task_6.py
@@ -1,42 +1,42 @@
 def num0():
     num = 0
-    print(num)
+    print(num, end='')
 
 def num1():
     num = 1
-    print(num)
+    print(num, end='')
 
 def num2():
     num = 2
-    print(num)
+    print(num, end='')
 
 def num3():
     num = 3
-    print(num)
+    print(num, end='')
 
 def num4():
     num = 4
-    print(num)
+    print(num, end='')
 
 def num5():
     num = 5
-    print(num)
+    print(num, end='')
 
 def num6():
     num = 6
-    print(num)
+    print(num, end='')
 
 def num7():
     num = 7
-    print(num)
+    print(num, end='')
 
 def num8():
     num = 8
-    print(num)
+    print(num, end='')
 
 def num9():
     num = 9
-    print(num)
+    print(num, end='')
 
 num8()
 num8()
